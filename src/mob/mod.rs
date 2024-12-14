077f2c24da7952941587fd2fe8768033f4526976
jonas <jonaspeitz@proton.me>
2024-12-14 14:17:38 +0100

    4

---
+++ src/mob/mod.rs
@@ -0,0 +1,18 @@
+
+use avian2d::{math::*, prelude::*};
+use bevy::prelude::*;
+use bevy_color::palettes::css::{ORANGE, WHITE};
+
+
+
+pub struct MobPlugin{
+    
+}
+
+
+impl Plugin for MobPlugin {
+    fn build(app:&mut app) {
+        
+    }
+    
+} 
